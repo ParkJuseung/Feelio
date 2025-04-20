e0ad849d2eaa84dcf046f98f1ef4fe1a997e81e7
ParkJuseung <judang0322@gmail.com>
2025-04-21 01:16:50 +0900

    feat : Google Login

---
+++ src/main/java/com/test/feelio/oauth/OAuthAttributes.java
@@ -0,0 +1,54 @@
+package com.test.feelio.oauth;
+
+import com.test.feelio.entity.Role;
+import com.test.feelio.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String nickname;  // name에서 nickname으로 변경
+    private String email;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String nickname, String email) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.nickname = nickname;  // name에서 nickname으로 변경
+        this.email = email;
+    }
+
+    // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 함
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        // 구글 로그인
+        if ("google".equals(registrationId)) {
+            return ofGoogle(userNameAttributeName, attributes);
+        }
+
+        // 추후 네이버, 카카오 로그인 추가 시 구현
+        return ofGoogle(userNameAttributeName, attributes); // 기본값은 구글로 설정
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nickname((String) attributes.get("name"))  // name에서 nickname으로 변경
+                .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity(String provider) {
+        return User.builder()
+                .nickname(nickname)  // name에서 nickname으로 변경
+                .email(email)
+                .role(Role.User)
+                .provider(provider)
+                .providerId((String) attributes.get(nameAttributeKey))
+                .build();
+    }
+}
