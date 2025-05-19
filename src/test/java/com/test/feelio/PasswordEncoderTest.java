067b633f44d119b5f2d2e134bbf0aa18ec17abc1
dnjs0 <heatherray@naver.com>
2025-05-19 23:42:53 +0900

    비밀번호 일치 단위테스트

---
+++ src/test/java/com/test/feelio/PasswordEncoderTest.java
@@ -0,0 +1,26 @@
+package com.test.feelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderTest {
+
+    @Test
+    void 비밀번호_매치_테스트() {
+        System.out.println("테스트 시작");
+        try {
+            String raw = "wwww1111";
+            String hash = "$2a$10$RzJPK9KuVb4pY1UZZJdSke0Muu2wYdN7dwBEdNp854EVaoVgG2bdW";
+
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+            boolean result = passwordEncoder.matches(raw, hash);
+            System.out.println("매치 결과: " + result);
+        } catch (Exception e) {
+            System.out.println("예외 발생!");
+            e.printStackTrace();
+        }
+        System.out.println("✅ 테스트 종료됨");
+    }
+}
